73df408a887ee2dbd83df5da8fb15e21aabc1c25
cskings14 <cskings14@gmail.com>
2022-10-20 03:20:03 -0400

    fixed navbar link

--- comps/Navbar.jsx
+++ comps/Navbar.jsx
@@ -1,19 +1,19 @@
 import Link from 'next/link';
 
 const Navbar = () => {
-    return ( 
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    return (
+        <nav>
             <div className="Name">
                 <h1>Christian King</h1>
             </div>
-
-            <Link href="/about"><a>About</a></Link>
-            <Link href="/contact"><a>Contact</a></Link>
-            <Link href="https://github.com/cskings14"><a>Repository</a></Link>
-            <Link href="/tutorial"><a>Tutorial</a></Link>
-
+            <div>
+                <Link href="/about"><a>About</a></Link>
+                <Link href="/contact"><a>Contact Me</a></Link>
+                <Link href="https://github.com/cskings14"><a>GitHub</a></Link>
+                <Link href="/tutorial"><a>Tutorial</a></Link>
+            </div>
         </nav>
-     );
+    );
 }
- 
+
 export default Navbar;
