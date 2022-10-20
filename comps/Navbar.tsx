b282315ff9e9a8c768929a8b5d1c8f682aa717b3
cskings14 <cskings14@gmail.com>
2022-10-20 03:59:08 -0400

    added global styles

--- comps/Navbar.tsx
+++ comps/Navbar.tsx
@@ -3,15 +3,15 @@ import Link from 'next/link';
 const Navbar = () => {
     return (
         <nav>
-            <div className="Name">
+            <div className="logo">
                 <h1>Christian King</h1>
             </div>
-            <div>
+            
                 <Link href="/about"><a>About</a></Link>
                 <Link href="/contact"><a>Contact Me</a></Link>
                 <Link href="https://github.com/cskings14"><a>GitHub</a></Link>
                 <Link href="/tutorial"><a>Tutorial</a></Link>
-            </div>
+            
         </nav>
     );
 }
