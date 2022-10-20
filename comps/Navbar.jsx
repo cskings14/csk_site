7f71f414973c7ad8354e49d33ba522665b3032ef
cskings14 <cskings14@gmail.com>
2022-10-20 02:57:18 -0400

    added code to footer/nav and delete index.tsx

---
+++ comps/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="Name">
+                <h1>Christian King</h1>
+            </div>
+
+            <a>About</a>
+            <a>Contact</a>
+            <a>Repository</a>
+            <a>Tutorial</a>
+
+        </nav>
+     );
+}
+ 
+export default Navbar;
