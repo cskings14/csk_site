9df9a2d4b2d57178806a1a4f99e6994bce1a0fac
cskings14 <cskings14@gmail.com>
2022-10-27 05:08:20 -0400

    deleted / created files for a spa

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,22 @@
-// import type { NextPage } from 'next'
 import Head from 'next/head'
-// import Image from 'next/image'
-// import styles from '../styles/Home.module.css'
-import Navbar from '../comps/Navbar'
-import Footer from '../comps/Footer'
 import styles from '../styles/Home.module.css'
 import Spline from '@splinetool/react-spline';
+// import About, Contact, Projects, Title from '../comps/sections/'
 
 const Home = () => {
   return (
     <>
-
     <Head>
-      <title> CK Portfolio | Home </title>
-      
+      <title> CHristian K </title>
     </Head>
+
+
     <div className={styles.container}>
-      {/* <Navbar /> */}
       <h1>Homepage</h1>
       <div className={styles.div}>
       <Spline scene="https://prod.spline.design/1OZ7ne96bkvlkwAj/scene.splinecode" />
       </div>
       <p>BLAH BLAH BLAH</p>
-
-      {/* <Footer /> */}
     </div>
 
     </>
--- pages/tutorial/index.tsx
+++
@@ -1,9 +0,0 @@
-const Index = () => {
-    return ( 
-        <div>
-            <h1>Added base tutorial page</h1>
-        </div>
-     );
-}
- 
-export default Index;
