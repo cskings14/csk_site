c0a137ff817ee496bd86d1293e5bf9cd8aa038f4
cskings14 <cskings14@gmail.com>
2022-10-27 04:55:11 -0400

    added icons, edit readme, added spline

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Head from 'next/head'
 import Navbar from '../comps/Navbar'
 import Footer from '../comps/Footer'
 import styles from '../styles/Home.module.css'
+import Spline from '@splinetool/react-spline';
 
 const Home = () => {
   return (
@@ -17,7 +18,11 @@ const Home = () => {
     <div className={styles.container}>
       {/* <Navbar /> */}
       <h1>Homepage</h1>
+      <div className={styles.div}>
+      <Spline scene="https://prod.spline.design/1OZ7ne96bkvlkwAj/scene.splinecode" />
+      </div>
       <p>BLAH BLAH BLAH</p>
+
       {/* <Footer /> */}
     </div>
 
