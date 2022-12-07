92ab17cac0afabbd74ca4673ece65ae22013d8b1
cskings14 <cskings14@gmail.com>
2022-12-07 06:22:23 -0500

    fixed the footer

--- comps/Footer.tsx
+++ comps/Footer.tsx
@@ -1,13 +1,17 @@
-const Footer = () => {
-    return (
-        <footer>
-            <ul className="footer-list">
-                <li>Coffee</li>
-                <li>Tea</li>
-                <li>Milk</li>
-            </ul>
-        </footer>
-    );
-}
+import Image from "next/image";
+import Pdf from "../Resumeex.pdf";
+
+
+const Footer = () => (
+    <footer>
+        <ul className="footer-list">
+            <a href="https://www.linkedin.com/in/cskings/"><Image src="/linkedin.png" width={50} height={50} /></a>
+            <a href="https://github.com/cskings14"><Image src="/github.png" width={50} height={50} /></a>
+            <a href={Pdf} rel="noopener noreferrer" target="_blank">
+            <Image src="/resume.png" width={50} height={50} />
+            </a>
+        </ul>
+    </footer>
+)
 
 export default Footer;
