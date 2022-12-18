a9261df48dadac538aa1bf757fd76c7a04de4dc6
cskings14 <cskings14@gmail.com>
2022-12-18 06:38:37 -0500

    deleted bruh added new spline

--- comps/sections/Title.tsx
+++ comps/sections/Title.tsx
@@ -7,7 +7,7 @@ import { motion } from 'framer-motion';
 const Title = () => {
     return ( 
         <div id='title'>
-                <Spline width={500} height={500} className={styles.spline} scene="https://prod.spline.design/1OZ7ne96bkvlkwAj/scene.splinecode" />
+                <Spline width={750} height={500} className={styles.spline} scene="https://prod.spline.design/i2qWHbrOX5xoo-Ut/scene.splinecode" />
                 <motion.div className={styles.name}>Hello, I am Christian King <span className={styles.wave}>&#9995;</span></motion.div>
                 <motion.div className={styles.job}>Fullstack Developer (Django / React)</motion.div>
                 <div className={styles.grid}>
