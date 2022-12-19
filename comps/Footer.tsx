4a8a5f0a6ab00fa393bba68f59381bcc075484a5
cskings14 <cskings14@gmail.com>
2022-12-19 08:11:39 -0500

    added real resume

--- comps/Footer.tsx
+++ comps/Footer.tsx
@@ -5,14 +5,14 @@ import Image from "next/image";
 const Footer = () => {
     const onButtonClick = () => {
         // using Java Script method to get PDF file
-        fetch('../res.pdf').then(response => {
+        fetch('../csking-res.pdf').then(response => {
             response.blob().then(blob => {
                 // Creating new object of PDF file
                 const fileURL = window.URL.createObjectURL(blob);
                 // Setting various property values
                 let alink = document.createElement('a');
                 alink.href = fileURL;
-                alink.download = 'res.pdf';
+                alink.download = 'csking-res.pdf';
                 alink.click();
             })
         })
