826896aeb372c56e04ad1d144e06d32d4f5661d0
cskings14 <cskings14@gmail.com>
2022-12-19 01:54:19 -0500

    added spline and named my boy craig

--- comps/sections/Title.tsx
+++ comps/sections/Title.tsx
@@ -1,20 +1,25 @@
-import Image from "next/image";
 import styles from "../../styles/Title.module.css"
 import Spline from '@splinetool/react-spline';
 import { motion } from 'framer-motion';
 
 
+
 const Title = () => {
-    return ( 
+    return (
         <div id='title'>
-                <Spline width={750} height={500} className={styles.spline} scene="https://prod.spline.design/i2qWHbrOX5xoo-Ut/scene.splinecode" />
-                <motion.div className={styles.name}>Hello, I am Christian King <span className={styles.wave}>&#9995;</span></motion.div>
-                <motion.div className={styles.job}>Fullstack Developer (Django / React)</motion.div>
-                <div className={styles.grid}>
-                    {/*This is where we are going to have both what I code and a small picture*/}
-                </div>
+            <div className={styles.craig}>Say hi to Craig, The Portfolio Mascot!</div>
+            <Spline width={750} height={500} className={styles.spline} scene="https://prod.spline.design/i2qWHbrOX5xoo-Ut/scene.splinecode" />
+            <motion.div className={styles.name}
+                whileTap={{
+                    scale: 0.8,
+                    rotate: -360,
+                    borderRadius: "100%"
+                  }}>Hello, I am Christian King <span className={styles.wave}>&#9995;</span></motion.div>
+            <motion.div className={styles.job}
+                initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 6 }}
+            >Fullstack Developer (Django / React)</motion.div>
         </div>
-     );
+    );
 }
- 
+
 export default Title;
